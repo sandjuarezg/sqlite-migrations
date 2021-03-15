733166eb763efe53db84147392069a821b5b1c46
Sandra <lissetdovah@gmail.com>
2021-03-15 02:52:52 -0600

    check exec

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type user struct {
+	id       int
+	name     string
+	username string
+}
+
+func main() {
+	sqlMigration()
+	insertData()
+	readData()
+}
+
+func insertData() {
+	var db, err = sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	statement, err := db.Prepare("INSERT INTO user (name, username) VALUES (?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer statement.Close()
+	statement.Exec("Dante Ramos", "dante123")
+}
+
+func readData() {
+	var db, err = sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, name, username FROM user")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var user = user{}
+
+	fmt.Printf("|%-6s|%-15s|%-15s|\n", "id", "Name", "User name")
+	fmt.Println("________________________________________")
+	for rows.Next() {
+		rows.Scan(&user.id, &user.name, &user.username)
+		fmt.Printf("|%-6d|%-15s|%-15s|\n", user.id, user.name, user.username)
+	}
+}
+
+func sqlMigration() {
+	var file, err = os.Open("./migration.sql")
+	if err != nil {
+		log.Fatal("File not found")
+	}
+	defer file.Close()
+
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var sqlQuerys = strings.Split(string(content), ";")
+
+	statement, err := db.Prepare(sqlQuerys[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer statement.Close()
+	statement.Exec()
+
+	statement, err = db.Prepare(sqlQuerys[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer statement.Close()
+	statement.Exec("Jose Diaz", "Admin")
+
+}
---
+++ migration.sql
@@ -0,0 +1,2 @@
+CREATE TABLE user (id INTEGER PRIMARY KEY, name TEXT, username TEXT);
+INSERT INTO user (name, username) VALUES (?, ?)
